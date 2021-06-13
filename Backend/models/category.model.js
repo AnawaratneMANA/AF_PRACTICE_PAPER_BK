c63474def5271e42fe1a905e28e1b8dc660f8aaa
AnwaratneMANA <it19086408@my.sliit.lk>
2021-06-13 14:44:30 +0530

    Model Classes Created

--- Backend/models/category.model.js
+++ Backend/models/category.model.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-const CategorySchema = new mongoose.Schema({
+const LoadSchema = new mongoose.Schema({
+    code: {type: String, required: true, trim: true},
     name: {type: String, required: true, trim: true},
-    price: {type: Number, required: true},
+    load: {type: String, required: true, trim: true},
+    ampkm: {type: Number, required: true},
     vehicles: [{type: mongoose.Schema.Types.ObjectId, required: false, ref:'vehicle'}],
 
 });
 
-const Category = mongoose.model('category', CategorySchema);
-module.exports = Category;
+const Load = mongoose.model('load', LoadSchema);
+module.exports = Load;
